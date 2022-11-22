aafabd1e2f54765bbd3227ff1d7dc3edc56f36c3
Shaz-M <shazmaradya@yahoo.com>
2022-11-22 02:50:24 -0600

    added google maps api functionality

--- front-end/src/pages/Locations.js
+++ front-end/src/pages/Locations.js
@@ -1,12 +1,14 @@
 import { useMemo } from "react";
-import { GoogleMap, useLoadScript, Marker } from  "@react-google-maps/api"
+import { GoogleMap, useLoadScript, Marker, MarkerF } from  "@react-google-maps/api"
 import "../styles/Map.css"
 import React, { useState, useEffect } from "react";
+import { getNearbyLocations } from "../services/menuService";
 
 
 export default function Locations(){
     const [latitude, setLatitude] = useState(0);
     const [longitude, setLongitude] = useState(0);
+    const [locations, setLocations] = useState([]);
 
     const { isLoaded } = useLoadScript({
         googleMapsApiKey: "AIzaSyA8oguzweCmBK3KUJTIckl3rdRO17iKQjg"
@@ -15,22 +17,56 @@ export default function Locations(){
     
 
     useEffect(() => {
+
         if ("geolocation" in navigator) {
             navigator.geolocation.getCurrentPosition(function(position) {
                 setLatitude(position.coords.latitude);
                 setLongitude(position.coords.longitude);
-                console.log(latitude);
-              });
-          } else {
+
+                });
+            } else {
             setLatitude(0);
             setLongitude(0);
-          }
+            }
+    
+
       },[]);
 
+      useEffect(() => {
+        getNearbyLocations(latitude,longitude).then(response => {
+            setLocations(response.locations);
+            });
+      },[latitude,longitude]);
+
+
+
+
     if(!isLoaded) return <div>Loading...</div>
+
+    if(locations.length!==0){
+        return(
+            <div style={{height:"100vh",width:"100%"}}>
+                <GoogleMap zoom={13} center = {{lat:latitude, lng: longitude}} mapContainerClassName="map-container">
+                    {locations.map((item,index) => {
+                    
+                        return (
+                            <MarkerF position={{lat:item.geometry.location.lat,lng:item.geometry.location.lng}}/>
+                                )
+                        
+                    })}
+
+                </GoogleMap>
+
+                
+
+
+            </div>
+        )
+    }
     return(
         <div style={{height:"100vh",width:"100%"}}>
             <GoogleMap zoom={13} center = {{lat:latitude, lng: longitude}} mapContainerClassName="map-container"></GoogleMap>
+
         </div>
     )
 }
