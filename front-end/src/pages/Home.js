441e0472dd99e77f3e437da76dccb5dfa9811765
Shaz-M <shazmaradya@yahoo.com>
2022-12-04 22:35:51 -0600

    fixed duplicate google translate

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -3,8 +3,26 @@ import { Link } from "react-router-dom";
 import BannerImage from "../assests/chick-black-white.jpg";
 import "../styles/Home.css";
 import GoogleTranslate from "../components/GoogleTranslate";
+import { useEffect,useState} from "react";
 
 function Home() {
+
+  let count = 0;
+
+  const googleTranslateElementInit = () => {
+    if(count === 0){
+      console.log("HERE");
+      new window.google.translate.TranslateElement({ pageLanguage: 'en', layout: window.google.translate.TranslateElement.FloatPosition.TOP_LEFT }, 'google_translate_element')
+    }
+    count++;
+   }
+   
+   useEffect(() => {
+     var addScript = document.createElement('script');
+     addScript.setAttribute('src', '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit');
+     document.body.appendChild(addScript);
+     window.googleTranslateElementInit = googleTranslateElementInit;
+   }, [])
   
   return (
     <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
@@ -14,7 +32,7 @@ function Home() {
         <Link to="/menu">
           <button> ORDER NOW </button>
         </Link>
-        <GoogleTranslate/>
+        <div id="google_translate_element"></div>
 
       </div>
     </div>
