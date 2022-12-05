8bd12fa6d20363a05cf7132e64770b050d3c2160
Lasyasri Shilpi <shilpilasya@tamu.edu>
2022-12-05 16:04:05 -0600

    Changes to main menu, front-end

--- front-end/src/pages/Menu.js
+++ front-end/src/pages/Menu.js
@@ -6,6 +6,7 @@ import '../styles/Menu.css';
 import { useEffect,useState } from "react";
 import Menu_Entrees from './Menu_Entrees';
 import { Link } from "react-router-dom";
+import '../styles/Menu.css';
 
 
 function myfunction() {
@@ -26,28 +27,33 @@ function Menu({handleClick}) {
 
     return (
       <div className='menu'>
-          <h1 className='menuTitle'> Our Menu</h1>
+          <h1 className='menuTitle_1'> Our Menu</h1>
           <div className='menuList'>
             <div className='entree'>
               <Link to="/menu/entrees">
-                  <button className='entree_button'> Entrees </button>
+                  <button className='entree_button'></button>
+                  <span>Entrees</span>
               </Link>
             </div>
             <div className='side'>
               <Link to="/menu/side">
-                <button className='side_button'> Side </button>
-              </Link>
-            </div>
-            <div className='drink'>
-              <Link to="/menu/drinks">
-                <button className='drink_button'> Drinks </button>
+                <button className='side_button'></button>
+                <span>Sides</span>
               </Link>
             </div>
             <div className='condiments'>
               <Link to="/menu/condiments">
-                <button className='condiment_button'> Condiments </button>
+                <button className='condiment_button'></button>
+                <span>Condiments</span>
               </Link>
             </div>
+            <div className='drink'>
+              <Link to="/menu/drinks">
+                <button className='drink_button'></button>
+                <span>Drinks</span>
+              </Link>
+            </div>
+            
 
             
           </div>
