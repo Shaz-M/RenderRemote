2efc6245035c92a090eacad283795d4173e7cee4
njck2001 <njck2001@tamu.edu>
2022-12-06 11:57:43 -0600

    reformatted About page

--- front-end/src/pages/About.js
+++ front-end/src/pages/About.js
@@ -4,9 +4,9 @@ export default function About() {
     return(
         <div className='menu'>
             <center> 
-                <h1 className='menuTitle_cart'> About </h1> 
+                <h1 className='menuTitle_1'> About </h1> 
             </center>
-            <center>
+            <center className='aboutText'>
                 The first Chick-fil-A® was founded in Hapeville, Georgia in 1946 by Truett Cathy. Truett believed it was important to close the restaurant on Sundays to give him and his employees a day of rest and the opportunity to worship. This is a tradition that every Chick-fil-A® restaurant continues to uphold to this day. Since the company’s founding, we have spread nationwide with current plans to go international! We are best known for our savory, breaded Chick-fil-A® Chicken Sandwiches and our friendly customer service. Most locations are open Monday through Saturday and serve breakfast from 6:00 a.m. to 10:30 a.m. and lunch and dinner from 10:30 a.m. to 10:00 p.m. It's our pleasure to serve you and your friends and family, so stop by whenever we’re open and remember to... <br/>
             <img src={Cows} alt="Eat More Chicken!"></img>
             </center>
